794574a1988c03b4a3260e2836d282108316c03d
Saw-Kyaw-Myint-SKM <Saw-Kyaw-Myint>
2023-08-13 20:18:00 +0630

    testing

--- app/src/registerServiceWorker.js
+++ app/src/registerServiceWorker.js
@@ -10,15 +10,7 @@ if (process.env.NODE_ENV === "production") {
   });
 
   register(`${process.env.BASE_URL}sw.js`, {
-    ready() {
-      console.log(
-        "App is being served from cache by a service worker.\n" +
-          "For more details, visit https://goo.gl/AFskqB"
-      );
-    },
-    registered() {
-      console.log("Service worker has been registered.");
-    },
+    registered() {},
     cached() {
       console.log("Content has been cached for offline use.");
     },
